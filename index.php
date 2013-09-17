52aeeecf1ba6e36502b62e02a41ba5e1368a766f
jbuldini <jbul707@gmail.com>
2013-09-17 15:10:01 -0400

    adding css

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-Welcome to P1, test 2222222222
-test
-test
-test
-test
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2//EN">
+<html>
+<head>
+<meta name="generator" content="HTML Tidy for Linux (vers 25 March 2009), see www.w3.org">
+<link rel="stylesheet" type="text/css" href="p1style.css" />
+<title>P1</title>
+</head>
+<body>
+<h2>Joe Buldini</h2>
+<p>My name is Joe and I live in southern New Hampshire with my dog Ringo.  I work in an IS group at a healthcare org in Boston.  My girlfriend Annie lives in Cambridge (not far from Harvard)</p>
+<p>As far as a code editor I'm currently using Notepad++.  In future assignments I'll probably be using Sublime Text 2 but I'd like familiarize myself with it first.</p>
+<p>My CSS will look better in future assignments.  I mean, it has too right...</p>
+</body>
+</html>
